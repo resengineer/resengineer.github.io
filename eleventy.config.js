1bb72bf2ab8745af59cb599480be5d7cf5c533b5
Dmitry Ivanov <dmitry.y.ivanov@me.com>
2026-08-07 11:43:51 +0400

    Add Eleventy local build and shared layout

---
+++ eleventy.config.js
@@ -0,0 +1,22 @@
+module.exports = function (eleventyConfig) {
+  // Общие статические файлы
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("videos");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("CNAME");
+
+  // Локальные материалы отдельных страниц
+  eleventyConfig.addPassthroughCopy("pages/**/images");
+  eleventyConfig.addPassthroughCopy("pages/**/videos");
+
+  // Старый фрагмент пока не участвует в сайте
+  eleventyConfig.ignores.add("includes/**");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site"
+    }
+  };
+};
