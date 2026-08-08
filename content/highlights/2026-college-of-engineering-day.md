f014b54108a612f7b6302a021ef45054c4ba413c
Dmitry Ivanov <dmitry.y.ivanov@me.com>
2026-08-08 11:14:55 +0400

    Add Highlights collection

---
+++ content/highlights/2026-college-of-engineering-day.md
@@ -0,0 +1,10 @@
+---
+title: College of Engineering Day 2026
+year: 2026
+order: 20
+tags: highlights
+permalink: false
+images:
+  - src: /images/SQU2026.jpg
+    alt: College of Engineering Day 2026
+---
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,4 @@
 module.exports = function (eleventyConfig) {
-  // Общие статические файлы
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("videos");
@@ -7,12 +6,22 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("CNAME");
   eleventyConfig.addPassthroughCopy("admin");
 
-  // Локальные материалы отдельных страниц
   eleventyConfig.addPassthroughCopy("pages/**/images");
   eleventyConfig.addPassthroughCopy("pages/**/videos");
 
-  // Старый фрагмент пока не участвует в сайте
   eleventyConfig.ignores.add("includes/**");
+
+  eleventyConfig.addCollection("highlights", function (collectionApi) {
+    return collectionApi
+      .getFilteredByTag("highlights")
+      .sort((a, b) => {
+        if (a.data.year !== b.data.year) {
+          return b.data.year - a.data.year;
+        }
+
+        return (a.data.order || 100) - (b.data.order || 100);
+      });
+  });
 
   return {
     dir: {
